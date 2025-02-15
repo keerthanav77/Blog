cc894bd1299f5b3d39a39a087e23ae9b0c4739d1
keerthanav77 <regikeerthana3@gmail.com>
2025-02-15 12:18:06 +0530

    cors

--- src/pages/api/index.js
+++ src/pages/api/index.js
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://blog-ashy-six-25.vercel.app/"],
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
